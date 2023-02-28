58ba10015b0cac9b25a73ed4b47ffdc3935e967f
José Joaquín Virtudes Castro <josejoaquinvirtudes@gmail.com>
2023-02-28 17:21:03 +0100

    Fix img url

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -121,6 +121,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
+    BASE_DIR / "detections",
 ]
 
 # Default primary key field type
--- dashboard/web/models.py
+++ dashboard/web/models.py
@@ -31,7 +31,7 @@ class Detector(models.Model):
 class Detection(models.Model):
     cam = models.ForeignKey("Cam",on_delete=models.SET_NULL,null=True,blank=True)
     date = models.DateTimeField(null=False,blank=False)
-    img = models.ImageField(upload_to='detections')
+    img = models.ImageField(upload_to='static/detections')
     items = models.CharField(max_length=100,null=True,blank=True)
     pred = models.CharField(max_length=100,null=True,blank=True)
     def __str__(self) -> str:
