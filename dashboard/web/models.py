acf28a9b9365f99c00bf3407874ba1d8c9a895b9
José Joaquín Virtudes Castro <101142795+JoaVirtudes19@users.noreply.github.com>
2023-06-07 19:43:36 +0200

    Merge Develop (#18)
    * Refactor some code and changed style
    
    * Changed html urls
    
    * Changed detail style
    
    * Changed detection style
    
    * Added edit cam
    
    * Fix detection list
    
    * Changed detection list
    
    * Changed base nav and create cam form
    
    * Refactor Telegram
    
    * Solved multiple detections Bug
    
    * Added user manual

--- dashboard/web/models.py
+++ dashboard/web/models.py
@@ -1,40 +1,46 @@
 from django.db import models
 from django.dispatch import receiver
 import os
+
+
 # Create your models here.
 
 
 ### TELEGRAM
 
 class Group(models.Model):
-    name = models.CharField(max_length=100,unique=True)
+    name = models.CharField(max_length=100, unique=True)
+
     def __str__(self) -> str:
         return str(self.name)
 
 
 class User(models.Model):
     name = models.CharField(max_length=100)
-    chat_id = models.CharField(max_length=100,unique=True)
-    group = models.ForeignKey(Group,on_delete=models.CASCADE,null=True,blank=True)
+    chat_id = models.CharField(max_length=100, unique=True)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, null=True, blank=True)
+
     def __str__(self) -> str:
         return str(self.name)
 
 
 ### DETECTOR
 class Detector(models.Model):
-    name = models.CharField(max_length=255,unique=True)
+    name = models.CharField(max_length=255, unique=True)
     model = models.FileField(upload_to='models')
+
     def __str__(self) -> str:
         return str(self.name)
-    
+
 
 class Detection(models.Model):
-    cam = models.ForeignKey("Cam",on_delete=models.SET_NULL,null=True,blank=True)
-    date = models.DateTimeField(null=False,blank=False)
+    cam =  models.CharField(max_length=100, null=True, blank=True)
+    date = models.DateTimeField(null=False, blank=False)
     img = models.ImageField(upload_to='static/detections')
-    items = models.CharField(max_length=100,null=True,blank=True)
-    pred = models.CharField(max_length=100,null=True,blank=True)
-    detector = models.ForeignKey(Detector,on_delete=models.SET_NULL,null=True,blank=True)
+    items = models.CharField(max_length=100, null=True, blank=True)
+    pred = models.CharField(max_length=100, null=True, blank=True)
+    detector =  models.CharField(max_length=100, null=True, blank=True)
+
     def __str__(self) -> str:
         if self.cam != None:
             return str(self.cam.name) + ": " + str(self.date)
@@ -45,45 +51,38 @@ class Detection(models.Model):
 ### CÁMARAS
 
 class Cam(models.Model):
-    name = models.CharField(max_length=100,unique=True)
-    detector = models.ForeignKey(Detector,on_delete=models.SET_NULL,null=True,blank=True)
+    name = models.CharField(max_length=100, unique=True)
+    detector = models.ForeignKey(Detector, on_delete=models.SET_NULL, null=True, blank=True)
     url = models.CharField(max_length=2048)
-    groups = models.ManyToManyField(Group,blank=True)
+    groups = models.ManyToManyField(Group, blank=True)
+
     def __str__(self) -> str:
         return str(self.name)
-
 
 
 ### Función que elimina el archivo en caso de ser eliminado su objeto
 @receiver(models.signals.post_delete, sender=Detector)
 def auto_delete_file_on_delete(sender, instance, **kwargs):
-
     try:
         if instance.model:
             if os.path.isfile(instance.model.path):
                 os.remove(instance.model.path)
-                print("Archivo "+str(instance.model.name)+" eliminado")
+                print("Archivo " + str(instance.model.name) + " eliminado")
             else:
-                print("Archivo "+str(instance.model.name)+" no existe")
+                print("Archivo " + str(instance.model.name) + " no existe")
     except:
-        ### Añadir a un log
-        print("Archivo "+str(instance.model.name)+" no ha podido ser eliminado")
+        print("Archivo " + str(instance.model.name) + " no ha podido ser eliminado")
+
 
 ### Función que elimina el archivo en caso de ser eliminado su objeto
 @receiver(models.signals.post_delete, sender=Detection)
 def auto_delete_file_on_delete(sender, instance, **kwargs):
-
     try:
         if instance.img:
             if os.path.isfile(instance.img.path):
                 os.remove(instance.img.path)
-                print("Archivo "+str(instance.img.path)+" eliminado")
+                print("Archivo " + str(instance.img.path) + " eliminado")
             else:
-                print("Archivo "+str(instance.img.path)+" no existe")
+                print("Archivo " + str(instance.img.path) + " no existe")
     except:
-        ### Añadir a un log
-        print("Archivo "+str(instance.img.path)+" no ha podido ser eliminado")
-
-
-
-
+        print("Archivo " + str(instance.img.path) + " no ha podido ser eliminado")
